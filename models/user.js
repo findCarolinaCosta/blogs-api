b65ef8a44a7848b2e443e5783b024d1aea3e2bdf
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-11 15:10:51 -0300

    Add Users model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: { 
+    type: DataTypes.INTEGER, 
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true, 
+  },
+  displayName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', Attributes,
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  return User;
+};
