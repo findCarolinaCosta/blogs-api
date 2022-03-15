b2c525d28228300fb4b494bdedfd1f7fb93904cb
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-15 18:38:18 -0300

    Refactor way to handle validation errors with joi

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,21 +1,4 @@
-// fonte base: monitoria 24.5 com o Gaspar
-// https://github.dev/tryber/sd-015-b-live-lectures/tree/monitoria/24.5
-
-const verify = (error, value) => 
-  typeof error.status === 'string' && error.status.includes(value);
-
 module.exports = (error, _req, res, _next) => {
   console.error(error);
-  const err = error;
-
-  if (verify(error, '"email" with value')) {
-    return res.status(400).json({ message: '"email" must be a valid email' });
-  }
-
-  if (verify(error, 'is not allowed to be empty')) {
-    err.message = error.status;
-    err.status = 400; 
-  }
-
-  return res.status(err.status || 500).json({ message: err.message });
+  return res.status(error.status || 500).json({ message: error.message });
 };
